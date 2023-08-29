799994f43ba2ed09eaaaa02a12836cbaebb142e4
trey_crow <treycrow@hey.com>
2023-08-28 20:43:24 -0500

    add tina

--- content/techs/trey.md
+++ content/techs/trey.md
@@ -1,17 +1,20 @@
 ---
-heroTitle: 
-heroContent: >
-  # Test content
+Name: Trey
+heroTitle: 'Hello World '
+heroContent: |
+  # Test content 
 
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  Lorem ipsum dolor sit amet, consectetur adipiscing elite
 heroButton: Contact me
 heroButtonURL: contact
 heroImage: images/hero/hero-trey.jpg
 heroVideoThumb: images/hero/popup-thumb.jpg
-heroVideoURL: https://www.youtube.com/watch?v=dQw4w9WgXcQ
-
-
-
-
-
+heroVideoURL: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+_template: tech
 ---
+
+
+
+
+
+
--- tina/config.js
+++ tina/config.js
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+  clientId: "cfbbbbcc-2bbe-4c4a-9903-dcd846aa130c", // Get this from tina.io
+  token: "04a94c9da95ebd0be936199f64519742ca715767", // Get this from tina.io
 
   build: {
     outputFolder: "admin",
@@ -21,25 +21,53 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
+        name: "techs",
+        label: "Technicians",
+        path: "content/techs",
+        format: "md",
+            templates: [
+              {
+              name: "tech",
+              label: "Technician",
+              format: "md",
+              fields: [
+                {
+                  name: "Name",
+                  lable: "Name",
+                  type: "string",
+                },
+                {
+                  name: "heroTitle",
+                  lable: "Hero Title",
+                  type: "string",
+                },
+                {
+                  name: "heroContent",
+                  lable: "Hero Content",
+                  type: "rich-text",
+                },
+                {
+                  name: "heroVideoURL",
+                  lable: "Video URL",
+                  type: "string",
+                },
+                {
+                  name: "heroImage",
+                  lable: "Image URL",
+                  type: "image",
+                },
+              ],
           },
         ],
       },
     ],
+  },
+  search: {
+    tina: {
+      indexerToken: 'aef0d6bb6ffa9aad6fe3b26466322130b088f4fa',
+      stopwordLanguages: ['eng']
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100
   },
 });
